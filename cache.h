d4347740edd55a94e46fd85b74de091e1ee01c53
Adam W Moran <amoran@unix1.andrew.cmu.edu>
2014-04-26 15:45:35 -0400

    Finished cache.h and started cache.c ... Made a Todo list.  Dat swag_cache tho.

--- cache.h
+++ cache.h
@@ -1,3 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "csapp.h"
+
+#define PARTITION_QUANTITY
+
+/* Each object forms a node in a linked list */
+struct cache_object {
+  size_t size;
+  char* uri;
+  unsigned int lru;
+  struct cache_object *next;
+};
+
+typedef struct cache_object cache_object;
+
+/* Each partition has a pointer to the first element in their linked list */
+struct cache_part {
+  size_t size;
+  int locked;
+  cache_object *first;
+};
+
+typedef struct cache_part cache_part;
+
+/* The main cache has an array of partition structs */
+struct cache_t {
+  size_t size;
+  cache_part parts[PARTITION_QUANTITY];
+};
+
+typedef struct cache_t cache_t;
