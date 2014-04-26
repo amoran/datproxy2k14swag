2e3f464b978ab210a152004200793690f4c03cdb
John Mann <jmannjr@unix6.andrew.cmu.edu>
2014-04-26 16:24:43 -0400

    Let's green this wood.

--- proxy.c
+++ proxy.c
@@ -1,3 +1,78 @@
+/*
+
+                    John Mann and Adam Moran Present
+                ____        _    ____           _             
+               |  _ \  __ _| |_ / ___|__ _  ___| |__   ___    
+               | | | |/ _` | __| |   / _` |/ __| '_ \ / _ \   
+               | |_| | (_| | |_| |__| (_| | (__| | | |  __/   
+               |____/ \__,_|\__|\____\__,_|\___|_| |_|\___|   
+              ____  _    _ _  _     ______        ___    ____ 
+             |___ \| | _| | || |   / ___\ \      / / \  / ___|
+               __) | |/ / | || |_  \___ \\ \ /\ / / _ \| |  _ 
+              / __/|   <| |__   _|  ___) |\ V  V / ___ \ |_| |
+             |_____|_|\_\_|  |_|   |____/  \_/\_/_/   \_\____|
+
+                                                                                
+                                                                                
+	                                                      ffttttttllffii                                                                        
+	                                                  ffttffffLLLLffttffttCCLLtt                                                                
+	                                                ttffCCCCCCttffLLCCCCCCffLLCCGG,,                                                            
+	                                            ttttllCCLLCCCCLLCCCCGGGGGGCCCCffLLGGCC                                                          
+	+--------------------------+            iittffllffLLffLLttLLGGGGCCGGCCCCCC@@LLGG@@@@                                                        
+	| I'm Canadian actor Bruce |            LLllffLLLLffLLffffCCGGGGCCCCLLCCLLLLGGLLGG@@@@                                                      
+	| Greenwood, and this is   |          ffffffttttLLLLLLttffCCLLffttttttttttffCCffGG@@@@GG                                                    
+	| simply the very best     |        llffttttffllLLttttLLffiittll;;;;;;iillttLLLLffGG@@@@                                                    
+	| web proxy ever created.  |        GGLLffffCCttttllttffff;;;;ii::::::;;llttLLttCCCCGG@@                                                    
+	|                          |        CCGGttLLllttttfffffftt;;::::::::::;;llttLLCCffGGGG@@                                                    
+	|                          |        GGCCttttllffffffLLllii;;::,,,,::::;;llttCCCCffGG@@@@                                                    
+	|                          |        CCCCLLllttllttiitt::ii,,,,,,::::::iillffCCCCLLGGGG@@                                                    
+	|                          |        ,,CCffttffttttll::,,,,,,,,,,::::::;;llttCCGGCC@@@@GG                                                    
+	+-----------------------+   \         CCLLttlltt;;::,,,,,,,,::,,,,,,::iillttLLGGGG@@GG@@                                                    
+	                         \   \        GGLLffllllii::::,,,,,,,,,,::::iiiiiillffGGGGGGGGCC                                                    
+	                          \   \       GGLLttii;;::::,,,,,,::::::;;;;iiiillttffGG@@GG@@CC                                                    
+	                           \   \      ffffttii;;::::::;;::::ii::iittttffLLLLCCGG@@GG@@CC                                                    
+	                            \   \   ,,ttff;;;;;;iiiiLLCCLLLL::,,ffGGGG@@GGGGGGGGGGGG@@ff                                                    
+	                             \   \  ,,::tt;;;;::llttttllii::::,,ttGG,,::iiLLffCCGGGG@@::                                                    
+	                              \   \   iill;;;;::::,,;;::,,,,::,,ttLL::::::iillttGGGG@@                                                      
+	                               \   \__..::;;ii::::,,,,,,..::::,,llCC;;;;;;iillLLGG@@@@                                                      
+	                                \_______::;;iiii::,,,,,,..,,::,,iiLLii::;;iittCCGG@@GG                                                      
+	                                        CC;;iiii;;::,,,,,,,,::..iiCCLL;;;;iiffGGGG@@tt                                                      
+	                                          ;;iiii;;::::,,::;;ttllCCGGGGii;;llLLGG@@                                                          
+	                                            llii;;::::::,,,,::ff@@@@GGttllttCCGG@@                                                          
+	                                            ,,ii;;;;;;::::::::ii;;llttttllffCCCCLL                                                          
+	                                              ll;;;;;;;;;;;;::::::llffGGffttCCCC::                                                          
+	                                              CC;;;;;;ll::iiiiiillttffGGttllCCGG                                                            
+	                                              ..ii;;;;;;::;;;;llttffGGffttLLLLGG                                                            
+	                                              lliiiiii;;;;;;;;;;llttttLLttCCGG;;                                                            
+	                                              @@ttllii;;;;::,,::;;iillffLLGGGG;;                                                            
+	                                          ..LL@@tt;;ttii;;::::iiiillffGGGGGG@@                                                              
+	                                          ffffCCii;;;;ttttllllffttffCCGG@@@@@@                                                              
+	                                        ::ffffCCii;;::;;ttCCCCGGGGGGGG@@@@@@@@@@..                                                          
+	                                      ffffffffffll;;;;::;;iittCCGGGGGG@@@@@@@@@@@@@@ff                                                      
+	                                iiLLffffffffffffCC;;;;;;;;iiiittCCGGGGGG@@@@@@GG@@@@@@GGLL                                                  
+	                          ..ffLLffffffffffffffffCC;;;;;;;;iiiittCCCCGG@@GGGG@@@@GG@@@@CCGGGGll                                              
+	                    ,,ffffffLLLLLLffffffffffffffffGG;;;;;;;;;;ttttLLLLCCGGGG@@GGGG@@GGLLCCCCGGLLLL::                                        
+	                  iiffLLffffCCffffffffffffffffffLL@@ii;;;;iilliittffttLLLLCCLLCCGGGGLLLLLLGGCCLLLLLLfftt                                    
+	              llLLffffffffffLLffLLffffffffffLL@@CCGGff;;;;;;iillttllllffttllllttLLLLffffffLLGGCCLLffLLLLffLL                                
+	          ..LLffffffffffffffLLffffffffffffff@@@@GGGGGG::::;;;;ii;;;;iill;;;;iillttCCffLLffLLCCCCLLffLLLLLLCCLL                              
+	        llLLffffffffffffffffLLffffffffffff@@@@GGLLCCGGtt::::::::::::ii;;;;;;iillffCCffffLLLLCCCCCCLLLLLLLLLLCCCCff                          
+	      ffffffffffffLLLLLLLLffffLLLLffffffGG@@CCffLLLL@@LL;;;;::::::;;;;::;;;;iiiiGGCCffffLL@@@@CCGGLLLLLLLLLLCCLLLLCC                        
+	    ttffffffLLLLLLLLLLLLLLffLLLLLLffffCC@@LLLLffffLLCCffLL;;;;::::::::;;::;;iiLLCCffffLLff@@@@@@GGGGLLLLLLLLCCLLCCLLCC                      
+	    ffffLLLLLLLLLLffLLLLLLffffLLLLffffffffLLffffffLLLLLLffCC::::::::::;;;;;;ffCCffffffff@@@@@@CCLLGGLLLLLLGGCCCCCCCCGGGG                    
+	  ffffffLLLLLLffLLffLLLLLLffffLLffffffLLffffffLLffffLLLLLLLLff::::::::;;;;iiffLLLLffLL@@@@@@@@CCffLLCCLLLLGGCCCCLLGGCCCCCC                  
+	  LLLLLLLLLLLLLLLLffLLLLLLLLffLLLLLLffffffLLLLLLffffffLLffffLL::::::::::;;ffLLLLffff@@@@@@@@GGLLLLCCCCLLLLGGCCGGLLCCLLGGCC..                
+	..LLLLLLLLCCLLffLLLLffLLLLLLffffLLffLLffffffLLLLffffLLLLLLLLLLCC::::::::llLLLLLLffGG@@@@CCLLLLLLCCCCLLLLCCGGLLCCCCGGLL@@LL@@CC              
+	LLLLLLLLLLCCLLLLLLLLffLLLLLLffffLLffffffffffLLLLLLffLLLLLLffLLLL;;;;::::ffLLLLff@@@@GGLLCCLLLLLLCCLLLLLLCCGGLLLLCCCCGGLLCCCCGG,,            
+	;;LLLLLLLLLLCCLLLLLLLLffffLLLLLLffffLLffffffffffLLLLffffLLLLffffLLtt::;;ffLLLLLL@@@@LLLLLLLLffLLCCCCLLLLLLGGCCLLCCGGCC@@LL@@LLCCCC            
+	..LLLLLLLLLLLLCCLLLLLLLLLLffLLLLLLffffLLLLffLLffffLLLLLLffLLLLLLLLGGLL::ttLLLL@@@@CCLLLLCCLLLLLLCCCCLLLLLLCC@@CCCCCCCCCC@@LL@@  
+                                                                                    
+
+Thanks, Bruce.
+
+This file creates the proxy.
+                                                                              
+*/
+
 #include <stdio.h>
 
 /* Include datproxy2k14swag headers. */
